d8781d887a0f041698627b6040d2cc7de4ae33b9
Hilton Meyer <development@hiltonmeyer.com>
2020-01-23 14:53:43 +0200

    Integrate auth0 login

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+import * as Auth from "./modules/auth/auth.mjs";
 import * as Alarm from "./modules/alarm/alarm.mjs";
-console.log(Alarm);
+
 ("use strict");
 
 // Data object
@@ -291,6 +292,17 @@ const clickDelegator = event => {
 // When the restart button is clicked, restart the timer
 document.addEventListener("click", clickDelegator, false);
 
+const buttonHandler = e => {
+  if (e.target.id === "btn-login") {
+    Auth.login();
+  }
+
+  if (e.target.id === "btn-logout") {
+    Auth.logout();
+  }
+};
+
+document.addEventListener("click", buttonHandler), false;
 renderTimer();
 renderTasks();
 Alarm.loadAlarm();
---
+++ modules/auth/auth.mjs
@@ -0,0 +1,103 @@
+console.log("Auth");
+
+let auth0 = null;
+const fetchAuthConfig = () => fetch("/auth_config.json");
+
+/**
+ * Configure the client
+ */
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+
+  auth0 = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId,
+  });
+};
+
+/**
+ * Update UI based on login status
+ */
+const updateUI = async () => {
+  try {
+    const isAuthenticated = await auth0.isAuthenticated();
+
+    document.getElementById("btn-logout").disabled = !isAuthenticated;
+    document.getElementById("btn-login").disabled = isAuthenticated;
+
+    console.log(`Authenticated: ${isAuthenticated}`);
+    if (isAuthenticated) {
+      document.getElementById("gated-content").classList.remove("hidden");
+
+      document.getElementById(
+        "ipt-access-token",
+      ).innerHTML = await auth0.getTokenSilently();
+
+      document.getElementById("ipt-user-profile").innerHTML = JSON.stringify(
+        await auth0.getUser(),
+      );
+    } else {
+      document.getElementById("gated-content").classList.add("hidden");
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+/**
+ * Login function
+ */
+const login = async () => {
+  await auth0.loginWithRedirect({
+    redirect_uri: window.location.origin,
+  });
+};
+
+/**
+ * Logout function
+ */
+const logout = () => {
+  auth0.logout({
+    returnTo: window.location.origin,
+  });
+};
+
+window.onload = async () => {
+  await configureClient();
+
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  if (isAuthenticated) {
+    console.log("> User is authenticated");
+    window.history.replaceState({}, document.title, window.location.pathname);
+    updateUI();
+    return;
+  }
+
+  console.log("> User not authenticated");
+
+  const query = window.location.search;
+  const shouldParseResult = query.includes("code=") && query.includes("state=");
+
+  if (shouldParseResult) {
+    console.log("> Parsing redirect");
+    try {
+      const result = await auth0.handleRedirectCallback();
+
+      if (result.appState && result.appState.targetUrl) {
+        showContentFromUrl(result.appState.targetUrl);
+      }
+
+      console.log("Logged in!");
+    } catch (err) {
+      console.log("Error parsing redirect:", err);
+    }
+
+    window.history.replaceState({}, document.title, "/");
+  }
+
+  updateUI();
+};
+
+export { login, logout };
