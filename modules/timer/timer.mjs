8855f05080e843613aa8a9bea83c4e87b653604b
Hilton Meyer <development@hiltonmeyer.com>
2020-05-21 11:13:57 +0300

    Reset timer after long rest period

--- modules/timer/timer.mjs
+++ modules/timer/timer.mjs
@@ -1,5 +1,7 @@
 'use strict';
 
+import * as Alarm from '../modules/alarm/alarm.mjs';
+
 const timerData = {
   timer: 1500,
   running: false,
@@ -10,7 +12,7 @@ let countDown;
 const timeout =
   window.location.hostname === 'localhost' ||
   window.location.hostname === '127.0.0.1'
-    ? 3
+    ? 1
     : 1000;
 let Alarm;
 
@@ -18,7 +20,6 @@ let Alarm;
  * Render the timer
  */
 const renderTimer = () => {
-   
   render('#timer', templateTimer);
   render('.pompoms', templatePompoms);
 
@@ -93,25 +94,46 @@ const clearInterval = () => {
 
 // Start Timer
 const startTimer = () => {
+  // Only start a new timer if one is not already running
   if (!countDown === undefined) return;
   countDown = window.setInterval(() => {
+    //Make sure the timer is running and not paused
     if (timerData.running) {
+      //  Check if the timer has ended, if it has determine the next course of action
       if (timerData.timer <= 0) {
         clearInterval();
         Alarm && Alarm.play();
+
+        /**
+         * Check the amount of pompoms, this will determine what happens to the timer
+         * The number of pompoms will indicate what iteration we are on
+         * 8: Will indicate the end of a round because this is the last iteration
+         * 7: Will be the long break after the previous 3 breaks
+         * 1,3,5: Will be the short breaks and can be calculated using mod
+         * 2,4,6: Pomodoro, this is the default not caught by any of the if statements
+         */
         timerData.pompoms++;
-        // Check the pompoms to determine next time
-        if (timerData.pompoms === 7) {
-          timerData.timer = 750;
+        console.log(timerData.pompoms);
+        // End: The 8th pompom will happen after the long break so requires it to be reset
+        if (timerData.pompoms === 8) {
           timerData.pompoms = 0;
-        } else if (timerData.pompoms % 2 === 1) {
-          // Short Break
+          timerData.timer = 1500;
+        }
+        // Long Break: The 7th pompom occurs after 4 rounds so this means you get a long break
+        else if (timerData.pompoms === 7) {
+          timerData.timer = 750;
+        }
+        // Short Break: Every 2nd pompom should be a break
+        else if (timerData.pompoms % 2 === 1) {
           timerData.timer = 300;
         } else {
           // Pomodoro period
           timerData.timer = 1500;
         }
       } else {
+        /**
+         * Decrease the timer
+         */
         timerData.timer--;
       }
     }
@@ -122,11 +144,10 @@ const startTimer = () => {
 /**
  * Setup the timer
  */
-const load = (alarm) => {
-    console.log(alarm);
-    Alarm = alarm;
-    renderTimer();
-}
+const load = () => {
+  renderTimer();
+  Alarm.load();
+};
 
 /**
  * Reset the timer
@@ -136,6 +157,9 @@ const reset = () => {
   timerData.timer = 1500;
   timerData.pompoms = 0;
   renderTimer();
+
+  // Kill the alarm if the button is pressed
+  Alarm.kill();
 };
 
 /**
@@ -145,6 +169,8 @@ const start = () => {
   timerData.running = true;
   startTimer();
   buttonAction.innerText = 'PAUSE';
+  // Kill the alarm if the button is pressed
+  Alarm.kill();
 };
 
 /**
@@ -153,6 +179,8 @@ const start = () => {
 const pause = () => {
   timerData.running = false;
   buttonAction.innerText = 'START';
+  // Kill the alarm if the button is pressed
+  Alarm.kill();
 };
 
 export { load, reset, start, pause };
