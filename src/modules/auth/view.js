3932fb9e0d70dd8bcd957885422727b3ba5a3409
Hilton Meyer <development@hiltonmeyer.com>
2020-06-02 19:41:42 +0300

    Update View

--- src/modules/auth/view.js
+++ src/modules/auth/view.js
@@ -1,29 +1,23 @@
 'use strict';
-const buttonLogin = document.querySelector('#btn-login');
-const todoistConfig = {
-  client_id: 'b95092242c9e4fc8b4a7c30c01c4bfe8',
-  scope: 'data:read,data:delete',
-  state: '0f34fc6173764b51b541204b57e55c7e',
-};
+// const buttonLogin = document.querySelector('#btn-login');
 
 const AuthView = {
   load: function () {
     console.log('Loading Auth');
-    if (!buttonLogin) {
-      throw Error('Login button missing');
-    }
+    // if (!buttonLogin) {
+    //   throw Error('Login button missing');
+    // }
 
-    /**
-     * Add event listener for click to add a new task
-     */
-    buttonLogin.addEventListener(
-      'click',
-      async function () {
-        console.log('Config', todoistConfig);
-        window.location = `https://todoist.com/oauth/authorize?client_id=${todoistConfig.client_id}&scope=${todoistConfig.scope}&state=${todoistConfig.state}&redirect_uri=localhost:1234`;
-      },
-      false,
-    );
+    // /**
+    //  * Add event listener for click to add a new task
+    //  */
+    // buttonLogin.addEventListener(
+    //   'click',
+    //   async function () {
+    //     console.log('Config', todoistConfig);
+    //   },
+    //   false,
+    // );
   },
 };
 
