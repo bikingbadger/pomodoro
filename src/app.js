b244d396907ed3d11d83ab677c8059fdb37e717c
Hilton Meyer <development@hiltonmeyer.com>
2020-06-07 17:37:39 +0300

    Import profile

--- src/app.js
+++ src/app.js
@@ -15,17 +15,27 @@ import ProfileModel from './modules/profile/model.mjs';
 AuthView.load();
 
 /**
+ * Use profile model and set task
+ */
+ProfileModel.load(PubSub);
+// If there are settings from the profile for todoist then add to the model
+if(ProfileModel.settings.todoistKey){
+    TasksModel.setTodistKey(ProfileModel.settings.todoistKey)
+};
+
+/**
  * Add the tasks view to the subscription of the pubSub
  * This will then receive the publications of the model each time a change is made
  */
 TasksView.load();
 PubSub.subscribe(TasksModel.subject, TasksView);
+
 /**
  * Setup the model to use the PubSub for publishing all changes
  * That way any subscribers will get the updates and make changes to the view
  */
-
 TasksModel.load(PubSub);
+
 /**
  * Add the timer view to the subscription of the pubSub
  * This will then receive the publications of the model each time a change is made
