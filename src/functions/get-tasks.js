ba5e29c00d58008154a28f80709f0d076f3ba76e
Hilton Meyer <development@hiltonmeyer.com>
2020-06-07 16:50:07 +0300

    Add parameter for authorization

--- src/functions/get-tasks.js
+++ src/functions/get-tasks.js
@@ -4,18 +4,18 @@ const allTaskURL = 'https://api.todoist.com/rest/v1/tasks?filter=today';
 
 /* export our lambda function as named "handler" export */
 exports.handler = async (event, context, callback) => {
+  // Check that an authorization token was sent
+  if (!event.headers.authorization) {
+    return callback(null, {
+      statusCode: 403,
+      body: JSON.stringify('Missing authorization token'),
+    });
+  }
   const response = await axios.get(allTaskURL, {
-    headers: { Authorization: `Bearer ${process.env.TODOIST_TOKEN}` },
+    headers: { Authorization: `${event.headers.authorization}` },
   });
   const tasks = await response.data;
-  console.log(tasks);
-
-  /* parse the string body into a useable JS object */
-  // console.log('======================================================');
-  // console.log(event);
-  // console.log(context);
-  // console.log('======================================================');
-
+  
   return callback(null, {
     statusCode: 201,
     body: JSON.stringify(tasks),
