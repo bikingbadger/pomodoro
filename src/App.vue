43d1243756c52c14f187da36e8fd626eea67cd95
Hilton Meyer <development@hiltonmeyer.com>
2021-01-17 22:19:15 +0200

    feat(setup): Init Vue

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
+</template>
+
+<style>
+
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+:root {
+  --clr-accent-500: #002fa7;
+}
+
+body {
+  font-family: "PT Sans", sans-serif;
+  font-size: 1.25rem;
+  font-weight: 400;
+}
+
+/* Utility classes */
+.flow-content > * + * {
+  margin-top: .75rem;
+}
+
+</style>
---
+++ src/components/timer/TimerBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flow-content">
+    <div class="timer-display">25:00</div>
+    <div class="pompoms">
+      <ul>
+        <li>🍅</li>
+        <li>🍅</li>
+        <li>🍅</li>
+      </ul>
+    </div>
+    <ul>
+      <li class="btn btn-go">Start</li>
+      <li class="btn btn-reset">Reset</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.timer-display {
+  text-align: center;
+  font-size: 2rem;
+}
+
+.pompoms {
+  text-align: center;
+}
+
+ul {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+ul > * + * {
+  margin-left: 0.5rem;
+}
+
+.btn {
+  padding: 0.5rem 1rem;
+  background: var(--clr-accent-500);
+  color: white;
+  border-radius: 1rem;
+}
+</style>
