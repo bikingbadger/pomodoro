44cbe8bd2acc786a27dc7c27027c754e5ea9328d
Hilton Meyer <development@hiltonmeyer.com>
2021-01-17 22:19:15 +0200

    feat(setup): Init Vue

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Pompom</h1>
+    <timer-bar></timer-bar>
+  </div>
+</template>
+
+<script>
+import TimerBar from "../components/timer/TimerBar";
+
+export default {
+  name: "Home",
+  components: { TimerBar }
+};
+</script>
