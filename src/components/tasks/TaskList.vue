8d07750d410c89fa7e0b8d4e58d36fa81b7a6f97
Hilton Meyer <development@hiltonmeyer.com>
2021-02-28 11:32:03 +0200

    feat(task): Create circles for list

--- src/components/tasks/TaskList.vue
+++ src/components/tasks/TaskList.vue
@@ -2,7 +2,7 @@
   <div class="task-list">
     <ul>
       <li v-for="task in allTasks" :key="task.id">
-        <span class="task-complete" @click="completeTask(task)"></span>
+        <div class="task-complete" @click="completeTask(task)"></div>
         <div>{{ task.description }}</div>
       </li>
     </ul>
@@ -24,38 +24,33 @@ export default {
 
 <style scoped>
 .task-list {
-  border: 1px solid blue;
+  /* border: 1px solid blue; */
   border-radius: 1rem;
   padding: 1rem;
   margin-top: 1rem;
-  box-shadow: 9px 10px 22px 0px rgba(0, 0, 0, 0.5);
+  box-shadow: 9px 10px 22px 0px rgba(97, 97, 97, 0.5);
 }
 
 li {
-  padding: 0.5em 0.5em 0.5em 2rem;
+  /* padding: 0.5em 0.5em 0.5em 2rem; */
   margin-bottom: 1rem;
   border-bottom: 1px solid var(--clr-accent-500);
   /* border-radius: 5px; */
-  position: relative;
-  display: flex;
-  align-content: center;
+  /* position: relative; */
+  display: grid;
+  grid-auto-flow: column;
+  grid-template-columns: 1fr, 5fr;
+  align-items: center;
+  justify-content: start;
 }
-
-/* ul > * + * {
-  margin-bottom: 0;
-} */
 
 .task-complete {
   content: '';
-  position: absolute;
   border-color: #009933;
   border-style: solid;
-  border-width: 0 0.3em 0.25em 0;
-  height: 1em;
-  top: 1.3em;
-  left: 0.6em;
-  margin-top: -1em;
-  transform: rotate(45deg);
-  width: 0.5em;
+  height: 20px;
+  width: 20px;
+  margin: 1rem;
+  border-radius: 50%;
 }
 </style>
