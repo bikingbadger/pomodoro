e1ac517a5c786c1bc6c25caf5c86d06050e3d8ed
Hilton Meyer <development@hiltonmeyer.com>
2021-03-17 08:46:33 +0200

    feat(task): Remove completed tasks that are unsynced

--- src/store/tasks/taskMutations.js
+++ src/store/tasks/taskMutations.js
@@ -1,6 +1,9 @@
 export default {
-  addTasks(state, tasks) {
-    tasks.forEach((task) => {
+  addTasks(state, todoistTasks) {
+    // Add any new tasks to list from Todoist
+    // and also remove any completed tasks that we updated
+    // in the Todoist app
+    todoistTasks.forEach((task) => {
       console.log(state);
       const taskInList = state.tasks
         ? state.tasks.find((element) => parseInt(element.sourceId, 10) === parseInt(task.id, 10))
@@ -24,6 +27,23 @@ export default {
       } else if (task.completed !== taskInList.completed) {
         taskInList.completed = task.completed;
         taskInList.scheduled = false;
+      }
+    });
+
+    // Remove any Todoist tasks from the store that we completed in the store
+    // but not synchronized
+    state.tasks.forEach((task, index) => {
+      const taskInList = todoistTasks.find(
+        (element) => parseInt(element.id, 10) === parseInt(task.sourceId, 10),
+      );
+
+      if (!taskInList) {
+        console.log(index, task, taskInList);
+        // Need to find the current index as deleting in a loop will change the
+        // index of the array as each one is deleted.
+        const currentTaskIndex = state.tasks.findIndex((element) => element.id === task.id);
+        console.log('Remove', currentTaskIndex, state.tasks[currentTaskIndex]);
+        state.tasks.splice(currentTaskIndex, 1);
       }
     });
   },
