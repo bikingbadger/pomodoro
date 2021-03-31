613c73197f1841b8ef8da5a81e6756f13507f93d
Hilton Meyer <development@hiltonmeyer.com>
2021-03-31 13:38:06 +0300

    fix(tasks): Remove commented out code

--- src/store/tasks/taskActions.js
+++ src/store/tasks/taskActions.js
@@ -1,20 +1,6 @@
-// import axios from 'axios';
-
-// const allTaskURL = 'https://api.todoist.com/rest/v1/tasks?filter=today';
-
 const todoistKey = process.env.VUE_APP_TODOISTKEY;
 
 export default {
-  // getTodoistTasks({ commit }) {
-  //   axios
-  //     .get(allTaskURL, { data: {}, headers: { Authorization: `Bearer ${todoistKey}` } })
-  //     .then((response) => {
-  //       commit('addTasks', response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // },
   addTasks({ commit }, payload) {
     commit('addTasks', payload);
   },
